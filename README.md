77746c1e2131b7cfc66b9eb7f492b4f486d2d78d
SimonYuan <simonyuan100@163.com>
2017-07-27 10:35:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# demo webpack
+
+### npm install -g webpack
+安装全局webpack
+### npm install -g webpack-dev-server
+安装全局webpack-dev-server
+
+### npm init
+初始化package.json
+
+
---
+++ main.js
@@ -0,0 +1,6 @@
+var data = {
+  name: 'SimonYuan',
+  age: 27
+}
+document.getElementById('name').innerHTML = data.name;
+document.getElementById('age').innerHTML = data.age;
